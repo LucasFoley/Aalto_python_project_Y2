33ec6101bc470ce8dfeb92d3beee7c050155a5fb
lukif <lucas.foley@aalto.fi>
2022-04-05 22:03:59 +0300

    Start window for GUI complete with play button

--- src/GUI.py
+++ src/GUI.py
@@ -2,51 +2,48 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 
-stylesheet = """
-        MainWindow {
-            background-image: url("C:/Users/lukif/OneDrive/Documents/Aalto/Programing Y2/Project/Images and backgrounds/castle_1.png")
-            no-repeat center center fixed;
-        }
-    """
-
 
 class StartWindow(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.show_image()
         self.push_play_button()
 
     def push_play_button(self):
         button = QPushButton("Play", self)
-        button.setGeometry(225, 600, 250, 60)
+        button.setGeometry(275, 700, 250, 60)
+        button.clicked.connect(self.play_button_clicked)
         layout = QVBoxLayout()
         layout.addWidget(button)
 
+    def play_button_clicked(self):
+        main_window = MainWindow()
+        main_window.show
 
-class ImageWidget(QWidget):
+    def show_image(self):
+        image = QPixmap("start_background.png")
+        label = QLabel(self)
+        label.setPixmap(image)
+
+
+class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
         self.show_image()
 
+        print("end")
+
     def show_image(self):
-        image = QPixmap("castle_1.png")
+        image = QPixmap("background.png")
         label = QLabel(self)
         label.setPixmap(image)
 
 
-class StartLayout(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-    def grid_layout(self):
-        grid = QGr
-
-
 def main():
     app = QApplication(sys.argv)
-    window = ImageWidget()
+    window = StartWindow()
     window.setWindowTitle("Dark Dungeon")
     window.resize(800, 800)
     window.show()
--- src/test_online_code.py
+++ src/test_online_code.py
@@ -1,31 +1,48 @@
 import sys
 from PyQt5.QtWidgets import *
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.centralwidget = QWidget()
-        self.setCentralWidget(self.centralwidget)
-
-        self.pushButton1 = QPushButton("Play", self.centralwidget)
-
-        layout = QVBoxLayout(self.centralwidget)
-        layout.addWidget(self.pushButton1)
-
+from PyQt5.QtGui import QPixmap
 
 stylesheet = """
-    MainWindow {
-        background-image: url("C:/Users/lukif/OneDrive/Documents/Aalto/Programing Y2/Project/Images and backgrounds/castle_1.png")
-        no-repeat center center fixed;
-    }
-"""
+        MainWindow {
+            background-image: url("C:/Users/lukif/OneDrive/Documents/Aalto/Programing Y2/Project/Images and backgrounds/start_background.png")
+            no-repeat center center fixed;
+        }
+    """
 
-if __name__ == "__main__":
+
+class StartWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.show_image()
+        self.push_play_button()
+
+    def push_play_button(self):
+        button = QPushButton("Play", self)
+        button.setGeometry(275, 700, 250, 60)
+        button.clicked.connect(self.play_button_clicked)
+        layout = QVBoxLayout()
+        layout.addWidget(button)
+
+    def play_button_clicked(self):
+        print("click")
+
+    def show_image(self):
+        image = QPixmap("start_background.png")
+        label = QLabel(self)
+        label.setPixmap(image)
+
+class MainWindow(QWidget)
+
+
+def main():
     app = QApplication(sys.argv)
-    app.setStyleSheet(stylesheet)  # <---
-    window = MainWindow()
-    window.resize(700, 700)
+    window = StartWindow()
+    window.setWindowTitle("Dark Dungeon")
+    window.resize(800, 800)
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
